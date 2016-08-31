d4cdaaf8051fdf4f19c90e40e2c5cdfef2212630
luxiaofeng <442171865@qq.com>
2016-08-31 11:16:53 +0800

    增加了可以添加图片的样式

--- BACustomAlertViewDemo/BACustomAlertViewDemo/ActionSheet/BACustomActionSheet.h
+++ BACustomAlertViewDemo/BACustomAlertViewDemo/ActionSheet/BACustomActionSheet.h
@@ -13,27 +13,41 @@
 
 #define BAWeak         __weak __typeof(self) weakSelf = self
 
+typedef NS_ENUM(NSInteger, BACustomActionSheetViewStyle) {
+    /**
+     *  普通样式
+     */
+    BACustomActionSheetNormal = 1,
+    /**
+     *  带标题样式
+     */
+    BACustomActionSheetTitle,
+    /**
+     *  带图片和标题样式
+     */
+    BACustomActionSheetImageAndTitle,
+    /**
+     *  带图片样式
+     */
+    BACustomActionSheetImage,
+};
+
 typedef void(^ButtonActionBlock)(NSInteger index);
 
-@interface BACustomActionSheet : UIView 
-
-/*!
- *  创建一个不带标题的ActionSheet
+@interface BACustomActionSheet : UIView
+/**
  *
- *  @param list              ActionSheet的各个选项
- *  @param clikckButtonIndex 按钮的点击事件
- */
-+ (void)ba_showActionSheetWithList:(NSArray *)list
-                 ClikckButtonIndex:(ButtonActionBlock)clikckButtonIndex;
-
-/*!
- *  创建一个带标题的ActionSheet
  *
- *  @param list              ActionSheet的各个选项
- *  @param title             标题
- *  @param clikckButtonIndex 按钮的点击事件
+ *  @param style             样式
+ *  @param list              选项数组(NSString数组)
+ *  @param sepcialIndex      特别颜色的下标数组(NSNumber数组)
+ *  @param title             标题内容(可空)
+ *  @param clikckButtonIndex block回调点击的选项
  */
-+ (void)ba_showActionSheetHaveTitleWithList:(NSArray *)list
-                                      title:(NSString *)title
-                          ClikckButtonIndex:(ButtonActionBlock)clikckButtonIndex;
++ (void)ba_showActionSheetHaveTitleWithViewStyle:(BACustomActionSheetViewStyle)style
+                                            List:(NSArray<NSString *> *)list
+                                            ImageList:(NSArray<UIImage *> *)imagelist
+                                         Special:(NSInteger)specialIndex
+                                           title:(NSString *)title
+                               ClikckButtonIndex:(ButtonActionBlock)clikckButtonIndex;
 @end
